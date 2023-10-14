a5c616f73894bad3ea1d8651e7074c681a5241a0
Pedro Emilio Martinelli <pedroemiliomartinelli@protonmail.com>
2023-10-14 14:38:48 -0300

    COMITADO

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LarguraAlturaRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r1 = new Retangulo();
+
+            Console.WriteLine("Entre com os dados do retangulo");
+            r1.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r1.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA: " + r1.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO: " + r1.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: " + r1.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+
+
+        }
+    }
+}
